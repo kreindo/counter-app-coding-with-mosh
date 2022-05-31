140425697f44036e3a0686d2c77f383fac04ce5f
ahmadsan <ahmadsan4p@gmail.com>
2022-05-31 16:20:23 +0800

    added counter component

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div className="m-2">
+        <h1>Button App</h1>
+        <button className="btn btn-primary btn-sm">
+          button
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
